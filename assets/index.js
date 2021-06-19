44138f91646d367e28d0b0ad45e42ce533c551c9
Carhul <carolinahulloen@gmail.com>
2021-06-19 12:42:22 +0000

    Changed the HTML for memory game and added JS instead. Deleted some CSS for the changes in HTML.

---
+++ assets/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cardArray = [
+        {
+            name: "couch",
+            img: "assets/memory_box_images/ben_unsplash.jpg"
+        }
+        {
+            name: "crossing road",
+            img: "assets/memory_box_images/daniel_cheung_unsplash.jpg"
+        }
+        {
+            name: "painting",
+            img: "assets/memory_box_images/daniel_cheung2_unsplash.jpg"
+        }
+        {
+            name: "couch",
+            img: "assets/memory_box_images/hello_im_nik_unsplash.jpg"
+        }
+        {
+            name: "couch",
+            img: "assets/memory_box_images/ines_pimentel_unsplash.jpg"
+        }
+        {
+            name: "couch",
+            img: "assets/memory_box_images/zakaria_ahada_unsplash.jpg"
+        }
+    ]
+})
