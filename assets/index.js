0337f48fb70bc763c81acde6ff016e44d17d50b5
Carhul <carolinahulloen@gmail.com>
2021-06-23 10:40:57 +0000

    Make the memorygame function properly

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,23 @@
 //Credit for getting the memory-game correct: Code with Ania Kubow, https://www.youtube.com/watch?v=tjyDOHzKN0w, Michelle3334 / freaky_memory on GitHub: https://github.com/Michelle3334
 // and dandavies23 / smoothie-moves on GitHub: https://github.com/dandavies23/smoothie-moves/blob/master/game.html
 
-//Lego cards in Memory Game - In the future more cards could be added
-    const LEGO_CARDS_LIST = [ //cardArray
+///The game will wait for the DOM to finish loading before running
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    //Cards in Memory Game
+    const cardArray = [
         {
             name: "couch",
             img: "assets/memory_box_images/ben_unsplash.jpg"
+        },
+        {
+            name: "couch",
+            img: "assets/memory_box_images/ben_unsplash.jpg"
+        },
+        {
+            name: "crossing road",
+            img: "assets/memory_box_images/daniel_cheung_unsplash.jpg"
         },
         {
             name: "crossing road",
@@ -16,6 +28,14 @@
             img: "assets/memory_box_images/daniel_cheung2_unsplash.jpg"
         },
         {
+            name: "painting",
+            img: "assets/memory_box_images/daniel_cheung2_unsplash.jpg"
+        },
+        {
+            name: "chicken",
+            img: "assets/memory_box_images/hello_im_nik_unsplash.jpg"
+        },
+        {
             name: "chicken",
             img: "assets/memory_box_images/hello_im_nik_unsplash.jpg"
         },
@@ -24,47 +44,41 @@
             img: "assets/memory_box_images/ines_pimentel_unsplash.jpg"
         },
         {
+            name: "unicorn",
+            img: "assets/memory_box_images/ines_pimentel_unsplash.jpg"
+        },
+        {
             name: "beach",
             img: "assets/memory_box_images/zakaria_ahada_unsplash.jpg"
-        }
+        },
+        {
+            name: "beach",
+            img: "assets/memory_box_images/zakaria_ahada_unsplash.jpg"
+        } 
     ];
 
-//The game will wait for the DOM to finish loading before running
+    //Sort the cards random
+    cardArray.sort(() => 0.5 - Math.random());
 
-document.addEventListener("DOMContentLoaded", () => {
+    //Constants
+    const memoryGame = document.querySelector("#memory-game");
+    const scoreDisplay = document.querySelector("#score");
+    const movesDisplay = document.querySelector("#moves");
+    const reset = document.getElementById("reset");
 
-    //Variables and empty arrays
-    let grid = document.querySelector(".memory-game");  //memorygame = now grid
     let cardsChosen = []
     let cardsChosenId = []
     let cardsCorrect = []
-    let rankMessage = "";
-    let finalScore = 0;
-    let turns = 0;
-    var score = 0;
-
-    // Doubles up cards to generate pairs
-    let legoCardsList = [...LEGO_CARDS_LIST, ...LEGO_CARDS_LIST]
-    let intervalRef = null;
-
-    // Defines elements on page
-    let alertDisplay = document.querySelector("#win-message")
-    let alertDisplay = document.querySelector("#result")
-    let movesDisplay = document.querySelector("#moves")
-    let resetButton = document.querySelector("#reset)"
-    resetButton.addEventListener("click");
 
     //Create Memory-game board
     function createMemory() {
-    //Sort the cards random
-    legoCardsList.sort(() => 0.5 - Math.random())
-        for (let i = 0; i < legoCardsList.length; i++) {
-            let card = document.createElement("img");
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement("img");
             card.setAttribute("src", "assets/memory_box_images/yellow_box.png");
-            card.setAttribute("data-id", i);
             card.setAttribute("class", "game-card");
-            card.addEventListener("click", onTumblerClick); //onTumblerClick = flipCard
-            grid.appendChild(card);
+            card.setAttribute("data-id", i);
+            card.addEventListener("click", flipCard);
+            memoryGame.appendChild(card);
         }
     }
 
@@ -134,6 +148,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     createMemory()
 })
+        
 
 //Contact-form 
 
